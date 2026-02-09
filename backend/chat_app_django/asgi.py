aef9ab0ec7d1ee0cb7469557011195b1c6f2ace5
asdAet <101425999+asdAet@users.noreply.github.com>
2026-02-09 19:08:18 +0500

    Update asgi.py

--- backend/chat_app_django/asgi.py
+++ backend/chat_app_django/asgi.py
@@ -17,7 +17,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','chat_app_django.settings')
 from django.core.asgi import get_asgi_application
 
 # ******************** #
-import chat.routing
+
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -30,6 +30,8 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app_django.settings')
 
 # application = get_asgi_application() # This must be replaced
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
